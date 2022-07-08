a5d629d8ad45888a7ccad2acd30629cd572c73ae
selamalka <99277476+selamalka@users.noreply.github.com>
2022-07-08 12:59:36 +0300

    Ouch Cloud
    Ouch Cloud

--- Supercool Antman - Project/Assets/Scripts/EffectManager.cs
+++ Supercool Antman - Project/Assets/Scripts/EffectManager.cs
@@ -1,23 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EffectManager : MonoBehaviour
 {
-    [SerializeField] GameObject dustEffect;
-    [SerializeField] Transform feetPosition;
-    PlayerInput playerInput;
-
-    private void Start()
-    {
-        playerInput = GetComponentInParent<PlayerInput>();
-    }
-
-    public void SpawnDust()
-    {
-        if (playerInput.isWalking)
-        {
-            Instantiate(dustEffect, feetPosition.position, Quaternion.identity);
-        }
-    }
 }
