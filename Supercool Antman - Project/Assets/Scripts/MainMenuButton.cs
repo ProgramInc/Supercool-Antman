afcb33ca844eb4bc499f53662fde79ecb8b95be1
selamalka <99277476+selamalka@users.noreply.github.com>
2022-07-11 14:39:09 +0300

    working on main menu
    working on main menu

--- Supercool Antman - Project/Assets/Scripts/MainMenuButton.cs
+++ Supercool Antman - Project/Assets/Scripts/MainMenuButton.cs
@@ -3,11 +3,14 @@ using UnityEngine.EventSystems;
 
 public class MainMenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    MainMenuManager mainMenuManager;
     Animator animator;
-
+    AudioSource audioSource;
     private void Start()
     {
         animator = GetComponent<Animator>();
+        audioSource = GetComponentInParent<AudioSource>();
+        mainMenuManager = GetComponentInParent<MainMenuManager>();
     }
 
     public void OnPointerExit(PointerEventData eventData)
@@ -18,5 +21,6 @@ public class MainMenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitH
     public void OnPointerEnter(PointerEventData eventData)
     {
         animator.SetBool("isMouseOnButton", true);
+        audioSource.PlayOneShot(mainMenuManager.mouseOverSound);
     }
 }
