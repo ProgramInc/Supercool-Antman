56486cb334d4360dfd1433fbd97bfb70f7b2a12f
selamalka <99277476+selamalka@users.noreply.github.com>
2022-07-12 22:25:15 +0300

    death screen working
    death screen working

---
+++ Supercool Antman - Project/Assets/Scripts/DeathButtonManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathButtonManager : MonoBehaviour
+{
+    [SerializeField] Image restartImage;
+    [SerializeField] Image menuImage;
+    [SerializeField] Button restartButton;
+    [SerializeField] Button menuButton;
+
+    private void OnEnable()
+    {
+        restartButton.onClick.AddListener(RestartGame);
+        menuButton.onClick.AddListener(BackToMainMenu);
+        PlayerStats.OnPlayerDeath += WrapperForActivateDeathButtons;
+    }
+
+    private void OnDisable()
+    {
+        PlayerStats.OnPlayerDeath -= WrapperForActivateDeathButtons;
+    }
+
+    private void Start()
+    {
+        restartImage.enabled = false;
+        menuImage.enabled = false;
+        restartButton.enabled = false;
+        restartImage.enabled = false;
+    }
+
+    private IEnumerator ActivateDeathButtons()
+    {
+        yield return new WaitForSeconds(5);
+
+        restartImage.enabled = true;
+        menuImage.enabled = true;
+        restartButton.enabled = true;
+        restartImage.enabled = true;
+
+        print("suppose to activate panel");
+    }
+
+    private void WrapperForActivateDeathButtons()
+    {
+        StartCoroutine(nameof(ActivateDeathButtons));
+    }
+
+    private void RestartGame()
+    {
+        print("restarting game");
+    }
+
+    private void BackToMainMenu()
+    {
+        print("back to main menu");
+    }
+}
