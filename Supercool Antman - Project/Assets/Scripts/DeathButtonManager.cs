71e4108cc38e6077f569d337e206f2a60d7d6225
selamalka <99277476+selamalka@users.noreply.github.com>
2022-07-12 22:48:57 +0300

    death buttons now functional
    death buttons now functional

--- Supercool Antman - Project/Assets/Scripts/DeathButtonManager.cs
+++ Supercool Antman - Project/Assets/Scripts/DeathButtonManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class DeathButtonManager : MonoBehaviour
 {
@@ -49,11 +50,12 @@ public class DeathButtonManager : MonoBehaviour
 
     private void RestartGame()
     {
-        print("restarting game");
+        SceneManager.LoadScene(1);
+        
     }
 
     private void BackToMainMenu()
     {
-        print("back to main menu");
+        SceneManager.LoadScene(0);
     }
 }
